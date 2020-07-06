036fd9ddb10eecac46857363e06f458ac530a937
Daniel Li <danyichingli@gmail.com>
2020-07-06 18:46:10 -0400

    Revert "Merge https://github.com/danyichingli/python_tetris_project"
    This reverts commit 68da9accee042f22d6aa29bf5a6f8a6bdb37ff02, reversing
    changes made to 355856b8de0acddb9b79db949b8995542a237113.

--- block.py
+++ block.py
@@ -5,14 +5,30 @@ from copy import deepcopy
 class Block:
 
     def __init__ (self, template):
-        self.template   = template
-        self.start_pos  = c.BLOCKS[self.template][0]
-        self.color      = c.BLOCKS[self.template][1]
-        self.curr_pos   = c.BLOCKS[self.template][0]
-        self.dropped    = False
+        # 'O', 'I', 'L', 'J', 'T', 'S', 'Z'
+        self.template           = template
+        # Positions on the grid
+        self.start_pos          = c.BLOCKS[self.template][0]
+        self.curr_pos           = c.BLOCKS[self.template][0]
+        # Color based on the template of block
+        self.color              = c.BLOCKS[self.template][1]
+        # Values for rotation functionality
+        self.rotation_states    = c.ROTATION[self.template]
+        self.curr_state         = 0
+        # If dropped, then use of this block is done
+        self.dropped            = False
 
     def get_color (self):
         return self.color
+
+    def get_rotation_states (self):
+        return self.rotation_states
+
+    def get_curr_state (self):
+        return self.curr_state
+
+    def set_curr_state (self, new_state):
+        self.curr_state = new_state
 
     def clone (self):
         new_block = deepcopy(Block(self.template))
--- gameController.py
+++ gameController.py
@@ -13,12 +13,16 @@ class Controller:
         self.window.pygame_init()
         self.gd.grid_generate()
         self.block_load()
-        #pg.mixer.init(44100, -16,2,2048)
+        pg.mixer.init(44100, -16,2,2048)
+        pg.mixer.music.load('Music/Tetris.mp3')
+        # TODO: consider adding the lines below to a 'settings' or 'music' class
+        pg.mixer.music.set_volume(0.1)
+        pg.mixer.music.play(-1)
 
     def game_loop (self):
         self.game_init()
         clock = self.gd.clock
-        while True:
+        while pg.mixer.music.get_busy():
             # Pygame loop speed
             time = clock.tick(c.FPS)
             self.gd.fall_time += time
@@ -33,64 +37,86 @@ class Controller:
         pg.quit()
 
     """Manual Changes"""
-    # Left movement
-    def move_left (self):
-        # Erase the block's current position, fill in new position to left
+    # Left/Right movement
+    def move (self, direction):
+        # Erase the block's current position, fill in new position to left/right
         new_grid = self.gd.get_grid()
+        self.block_erase(new_grid)
         for i in range(4):
             row = self.gd.curr_block.curr_pos[i][0]
             col = self.gd.curr_block.curr_pos[i][1]
-            self.gd.curr_block.curr_pos[i] = (row, col-1)
-            new_grid[row][col] = c.GREY
-            new_grid[row][col-1] = self.gd.curr_block.color
-        return new_grid
-
-    # Right movement
-    def move_right (self):
-        # Erase the block's current position, fill in new position to right
-        # ***If I used range(4), I'd be erasing the left side of the block in
-        # its new position.
-        new_grid = self.gd.get_grid()
-        for i in range(3,-1,-1):
-            row = self.gd.curr_block.curr_pos[i][0]
-            col = self.gd.curr_block.curr_pos[i][1]
-            self.gd.curr_block.curr_pos[i] = (row, col+1)
-            new_grid[row][col] = c.GREY
-            new_grid[row][col+1] = self.gd.curr_block.color
+            if direction == "left":
+                self.gd.curr_block.curr_pos[i] = (row, col-1)
+                new_grid[row][col-1] = self.gd.curr_block.color
+            else: # direction == "right"
+                self.gd.curr_block.curr_pos[i] = (row, col+1)
+                new_grid[row][col+1] = self.gd.curr_block.color
         return new_grid
 
     # Soft drop AKA "Down movement"
     def soft_drop (self):
         new_grid = self.gd.grid
-        for i in range(3,-1,-1):
+        self.block_erase(new_grid)
+        for i in range(4):
             row = self.gd.curr_block.curr_pos[i][0]
             col = self.gd.curr_block.curr_pos[i][1]
             self.gd.curr_block.curr_pos[i] = (row+1, col)
-            new_grid[row][col] = c.GREY
             new_grid[row+1][col] = self.gd.curr_block.color
         return new_grid
 
+    # Hard drop
     def hard_drop (self):
         new_grid = self.gd.grid
         while not self.block_collision_v():
-            for i in range(3,-1,-1):
+            self.block_erase(new_grid)
+            for i in range(4):
                 row = self.gd.curr_block.curr_pos[i][0]
                 col = self.gd.curr_block.curr_pos[i][1]
                 self.gd.curr_block.curr_pos[i] = (row+1, col)
-                new_grid[row][col] = c.GREY
                 new_grid[row+1][col] = self.gd.curr_block.color
         self.gd.curr_block.dropped = True
         return new_grid
 
     # Follows super rotation system. If it collides, then don't rotate.
-    def rotate_block (self):
+    def rotate (self, direction):
         # O-block doesn't rotate
-        # I-block, S-block, Z-block, L-block, J-block, T-block rotates 4 ways
-        # Check collision before committing rotation
-        if self.gd.curr_block.template == 'O':
+        if self.gd.curr_block.rotation_states == None:
             return self.gd.grid
+        # I-block, S-block, Z-block, L-block, J-block, T-block rotates 4 ways
         else:
-            return
+            # Check collision before committing rotation
+            new_grid = self.gd.get_grid()
+            curr_state = self.gd.curr_block.get_curr_state()
+            next_state = curr_state
+            diff = []
+            if direction == "clockwise":
+                if curr_state == 3:
+                    next_state = 0
+                else:
+                    next_state += 1
+            else: # direction == "counter clockwise"
+                if curr_state == 0:
+                    next_state = 3
+                else:
+                    next_state -= 1
+            curr_points = self.gd.curr_block.get_rotation_states()[curr_state]
+            next_points = self.gd.curr_block.get_rotation_states()[next_state]
+            if direction == "clockwise":
+                diff = self.find_diff(direction, curr_points, next_points)
+            else: # direction == "counter clockwise"
+                diff = self.find_diff(direction, next_points, curr_points)
+            if not self.block_collision_r(diff):
+                self.block_erase(new_grid)
+                for i in range(4):
+                    row = self.gd.curr_block.curr_pos[i][0]
+                    col = self.gd.curr_block.curr_pos[i][1]
+                    next_row = row + diff[i][0]
+                    next_col = col + diff[i][1]
+                    self.gd.curr_block.curr_pos[i] = (next_row, next_col)
+                    new_grid[next_row][next_col] = self.gd.curr_block.color
+            self.gd.curr_block.set_curr_state(next_state)
+            return new_grid
+
 
     """Automatic Changes"""
     # Load block into game where the top-left part of block is placed at (0,4)
@@ -102,7 +128,8 @@ class Controller:
             pg.quit()
 
     def block_fall (self):
-        if self.gd.fall_time / 1000 > self.gd.fall_speed:
+        # default = 1000
+        if self.gd.fall_time / 3500 > self.gd.fall_speed:
             self.gd.fall_time = 0
             if not self.block_collision_v():
                 return self.soft_drop()
@@ -137,7 +164,7 @@ class Controller:
     def event_listener (self):
         # Soft drop (key hold)
         if pg.key.get_pressed()[pg.K_DOWN] and not self.block_collision_v():
-            return self.soft_drop()
+            self.soft_drop()
         for event in pg.event.get():
             # Close window
             if event.type == pg.QUIT:
@@ -145,17 +172,21 @@ class Controller:
             elif event.type == pg.KEYDOWN:
                 # Move left (key press)
                 if event.key == pg.K_LEFT and not self.block_collision_h("left"):
-                    return self.move_left()
+                    self.move("left")
                 # Move right (key press)
                 elif event.key == pg.K_RIGHT and not self.block_collision_h("right"):
-                    return self.move_right()
+                    self.move("right")
                 # Hard drop (key press)
                 elif event.key == pg.K_SPACE and not self.block_collision_v():
-                    return self.hard_drop()
+                    self.hard_drop()
+                elif event.key == pg.K_x:
+                    self.rotate("clockwise")
+                elif event.key == pg.K_z:
+                    self.rotate("counter clockwise")
                 # Close window (key press)
                 elif event.key == pg.K_ESCAPE:
                     pg.quit()
-        return self.block_fall()
+        self.block_fall()
 
     def update (self):
         self.event_listener()
@@ -167,21 +198,17 @@ class Controller:
     # Check block collision horizontally
     def block_collision_h (self, side):
         temp_grid = self.gd.get_grid()
-        if side == "left":
-            for i in range(4):
-                pos = self.gd.curr_block.curr_pos
-                row = pos[i][0]
-                col = pos[i][1]
-                if (col == 0 or
-                        ((row,col-1) not in pos and temp_grid[row][col-1] != c.GREY)):
+        for i in range(4):
+            pos = self.gd.curr_block.curr_pos
+            row = pos[i][0]
+            col = pos[i][1]
+            if side == "left":
+                if (col == 0 or ((row,col-1) not in pos
+                and temp_grid[row][col-1] != c.GREY)):
                     return True
-        else:
-            for i in range(4):
-                pos = self.gd.curr_block.curr_pos
-                row = pos[i][0]
-                col = pos[i][1]
-                if (col == 9 or
-                        ((row,col+1) not in pos and temp_grid[row][col+1] != c.GREY)):
+            else: # if side == "right"
+                if (col == 9 or ((row,col+1) not in pos
+                and temp_grid[row][col+1] != c.GREY)):
                     return True
         return False
 
@@ -192,10 +219,19 @@ class Controller:
             pos = self.gd.curr_block.curr_pos
             row = pos[i][0]
             col = pos[i][1]
-            # Only focus on the positions with row that's downmost
-            if (row == 19 or
-                    ((row+1,col) not in pos and temp_grid[row+1][col] != c.GREY)):
+            if (row == 19 or ((row+1,col) not in pos
+            and temp_grid[row+1][col] != c.GREY)):
                 self.gd.curr_block.dropped = True
+                return True
+        return False
+
+    def block_collision_r (self, diff):
+        temp_grid = self.gd.get_grid()
+        for i in range(4):
+            pos = self.gd.curr_block.curr_pos
+            row = pos[i][0] + diff[i][0]
+            col = pos[i][1] + diff[i][1]
+            if row < 0 or row > 19 or col < 0 or col > 9:
                 return True
         return False
 
@@ -210,5 +246,26 @@ class Controller:
         return False
 
     """Miscellaneous"""
+    def block_erase (self, grid):
+        new_grid = grid
+        for i in range(4):
+            row = self.gd.curr_block.curr_pos[i][0]
+            col = self.gd.curr_block.curr_pos[i][1]
+            new_grid[row][col] = c.GREY
+        return new_grid
+
+    def find_diff (self, direction, curr_points, next_points):
+        result = []
+        for i in range(4):
+            curr_row = curr_points[i][0]
+            curr_col = curr_points[i][1]
+            next_row = next_points[i][0]
+            next_col = next_points[i][1]
+            if direction == "clockwise":
+                result.append((next_row - curr_row, next_col - curr_col))
+            else:
+                result.append((curr_row - next_row, curr_col - next_col))
+        return result
+
     def play_sound (self, sound):
         return
